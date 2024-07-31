7029716babafc5f4ae42cf0fcdfd821a1c9f6510
HugSTapG <hugo.tapia@uees.edu.ec>
2024-07-31 15:32:18 -0500

    Actions

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import os
 
 num = os.environ.get("INPUT_NUM")
-if num:
-	try:
-		num = int(num)
-	except Exception:
-		exit("El numero ingresado no es entero")
-else:
-	num = 1
 
-if "GITHUB_OUTPUT" in os.environ :
-	with open["GITHUB_OUTPUT"], "a") as f :
-		print("{0}={1}".format('result', num+3), file=f))
+if num:
+    try:
+        num = int(num)
+    except Exception:
+        exit("El numero ingresado no es entero")
+else:
+    num = 1
+
+if "GITHUB_OUTPUT" in os.environ:
+    with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+        print("{0}={1}".format('result', num+3), file=f))
